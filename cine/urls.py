1e7cdf55c640fac426dd05416ac92141856e11fc
llopez502 <93736978+llopez502@users.noreply.github.com>
2021-11-07 20:23:08 -0600

    Update urls.py

--- cine/urls.py
+++ cine/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cines.urls')),
+    path('admin/', admin.site.urls)
 ]
